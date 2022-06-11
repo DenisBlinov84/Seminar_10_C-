2001b8d7542752fa7336b2bea5d25c534530a44d
Denis <blinovda1@mail.ru>
2022-06-11 16:54:21 +0300

    add task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два числа и выдаёт
+//    первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+
+
+int AllEvenNumers(int num_1, int num_2, int n)
+{
+    if (n == 1) return num_1;
+    Console.Write($"{num_1} ");
+    return AllEvenNumers(num_2, num_1 + num_2, n - 1);
+}
+
+Console.WriteLine(AllEvenNumers(3, 4, 5));
+Console.WriteLine(AllEvenNumers(6, 10, 4));
