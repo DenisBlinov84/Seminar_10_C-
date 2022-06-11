668ec3110bcf90a3da197d8e25ab1b1447f0623d
Denis <blinovda1@mail.ru>
2022-06-11 16:57:05 +0300

    add task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Дано натуральное число N. Выведите все его цифры по одной,
+//    в обратном порядке, разделяя их пробелами или новыми строками.
+
+
+void LetterComb(char[] abc, char[] word, int N = 0)
+{
+    if (N == word.Length)
+    {
+        Console.WriteLine($"{new String(word)}"); return;
+    }
+    for (int i = 0; i < abc.Length; i++)
+    {
+        word[N] = abc[i];
+        LetterComb(abc, word, N + 1);
+    }
+}
+Console.Write("Enter the number N: ");
+int N = int.Parse(Console.ReadLine());
+char[] abc = { 'а', 'и', 'с', 'в' };
+LetterComb(abc, new char[N]);
